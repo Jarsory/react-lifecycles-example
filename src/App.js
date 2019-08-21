785a417259fa815764913f4e0675335e764d5f13
Jarsory <arsory92@gmail.com>
2019-08-20 20:52:37 -0500

    ciclo de vida de los componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Counter from './components/Counter'
+
+class App extends Component {
+  
+  state = {
+    isRunning: true
+  }
+
+  stopCounter = () => {
+      this.setState({...this.state, isRunning: false })
+  }
+
+  render() {
+    console.log('I am in RENDER of the App.js.');
+    return this.state.isRunning ? (
+      <div className="App">
+        <h2>Clicking this button will destroy the Counter component and clear the state.</h2>
+        <button onClick={this.stopCounter}>Stop the counter</button>
+
+        <Counter isRunning={this.state.isRunning} /> 
+      </div>
+    ) : 'Se ha detenido el contador'
+  }
 }
 
 export default App;
